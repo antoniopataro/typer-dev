5c2d3b010a28b951d3d6e2cd1778d5283cc65eae
antoniopataro <antonio.pataro12@gmail.com>
2022-07-05 09:04:51 -0300

    heroku

--- server/index.js
+++ server/index.js
@@ -11,5 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+app.use("/", (req, res) => {
+  res.send({ status: "App is running!" });
+});
+
 require("dotenv").config();
 app.listen(process.env.SERVER_PORT);
