dcb3b0d66bdb2efb07559fa2ed8596b5531eae50
antoniopataro <antonio.pataro12@gmail.com>
2022-07-05 08:51:05 -0300

    fix: config-files - changes for full-stack folder structure

---
+++ client/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig, searchForWorkspaceRoot } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/typer-dev/",
+  plugins: [react()],
+  server: {
+    fs: {
+      allow: [".."],
+    },
+  },
+});
